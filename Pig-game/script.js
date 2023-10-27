b20f51262011ea13e9574ff603fa849d729f73da
Siphesihle Dyalvane <115469219+lilanga96@users.noreply.github.com>
2023-10-27 09:55:19 +0200

    implementent switch player logic

--- Pig-game/script.js
+++ Pig-game/script.js
@@ -8,6 +8,8 @@ const newGame = document.querySelector('.btn--new');
 const holdGame = document.querySelector('.btn--hold');
 const currentScore0 = document.getElementById('current--0')
 const currentScore1 = document.getElementById('current--1')
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
 
 
 //starting conditions
@@ -19,6 +21,7 @@ dice.classList.add('hidden');
 diceHidden();
 
 let currentScore = 0;
+let activePlayer = 0;
 
 //functionality
 
@@ -30,6 +33,13 @@ rollDice.addEventListener('click', function(){
        dice.src = `dice-${randomDice}.png`;
 
        if(randomDice !== 1){
-        currentScore0 += randomDice;
+        currentScore += randomDice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+       }else{
+        currentScore = 0;
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player1.classList.toggle('player--active');
+        player2.classList.toggle('player--active');
        }
 })
