6da70fd8e1abc3a497212b823cc36091fe47fa3a
Siphesihle Dyalvane <115469219+lilanga96@users.noreply.github.com>
2023-10-27 09:00:56 +0200

    implemented game logic

--- Pig-game/script.js
+++ Pig-game/script.js
@@ -1,4 +1,35 @@
 'use strict';
+//selecting elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+const rollDice = document.querySelector('.btn--roll');
+const newGame = document.querySelector('.btn--new');
+const holdGame = document.querySelector('.btn--hold');
+const currentScore0 = document.getElementById('current--0')
+const currentScore1 = document.getElementById('current--1')
 
-const score0 = document.getElementById('#score--0');
-const score1 = document.getElementById('#score--1');
+
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+function diceHidden(){
+dice.classList.add('hidden');
+}
+diceHidden();
+
+let currentScore = 0;
+
+//functionality
+
+
+
+rollDice.addEventListener('click', function(){
+       const randomDice = Math.floor(Math.random()* 6)+1;
+       dice.classList.remove('hidden');
+       dice.src = `dice-${randomDice}.png`;
+
+       if(randomDice !== 1){
+        currentScore0 += randomDice;
+       }
+})
