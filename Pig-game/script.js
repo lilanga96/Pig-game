af7cad2f79123abdd9094c9c1c2190d1a12d3125
Siphesihle Dyalvane <115469219+lilanga96@users.noreply.github.com>
2023-10-27 11:49:43 +0200

    implemented logic to reset the game

--- Pig-game/script.js
+++ Pig-game/script.js
@@ -13,6 +13,7 @@ const player2 = document.querySelector('.player--1');
 
 
 //starting conditions
+const scores = [0,0];
 score0El.textContent = 0;
 score1El.textContent = 0;
 function diceHidden(){
@@ -22,12 +23,14 @@ diceHidden();
 
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 //functionality
 
 
 
 rollDice.addEventListener('click', function(){
+    if(playing){
        const randomDice = Math.floor(Math.random()* 6)+1;
        dice.classList.remove('hidden');
        dice.src = `dice-${randomDice}.png`;
@@ -42,14 +45,31 @@ rollDice.addEventListener('click', function(){
         player1.classList.toggle('player--active');
         player2.classList.toggle('player--active');
        }
+    }
 })
 
 holdGame.addEventListener('click', function(){
+    if(playing){
+       scores[activePlayer] += currentScore
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
 
-    document.getElementById(`score--${activePlayer}`).textContent = currentScore;
+        if(scores[activePlayer] >= 12){
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+            playing = false
+            diceHidden();
+        }else{
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player1.classList.toggle('player--active');
+        player2.classList.toggle('player--active');
+        }
+    }
 })
 
 newGame.addEventListener('click', function(){
+    playing = true;
     score0El.textContent = 0;
     score1El.textContent = 0;
     function diceHidden(){
@@ -57,6 +77,7 @@ newGame.addEventListener('click', function(){
 }
     diceHidden();
     document.getElementById(`current--${activePlayer}`).textContent = 0;
-     currentScore = 0;
+     activePlayer = 0;
+     document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner'); 
     
 })
